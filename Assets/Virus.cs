4ee2e8946f7c384a468f3e889f5ddd05c5a255f2
Jeremy Morel <jeremy.morel.jm@gmail.com>
2016-04-16 08:50:53 -0700

    wip

--- Assets/Virus.cs
+++ Assets/Virus.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class Virus : MonoBehaviour {
 
-
+	public AnimationCurve goOnCenterAnimation;
 	public float moveForce;
+	public int nbFrameAnimation;
+
+
+	private bool m_isOnCenterAnimation;
+	private Vector2 m_centerCell; 
+	private float m_animationTime;
+	private Vector3 toto;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,6 +19,11 @@ public class Virus : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (m_isOnCenterAnimation) {
+			Vector3 dif = -toto + new Vector3(m_centerCell.x,m_centerCell.y,this.transform.position.z);
+			this.transform.position = toto + dif*(goOnCenterAnimation.Evaluate(m_animationTime/nbFrameAnimation));
+			m_animationTime++;
+		}
 	
 	}
 
@@ -35,9 +47,14 @@ public class Virus : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Start Collide " + other.gameObject.layer);
 		if (other.gameObject.layer == LayerMask.NameToLayer("Cell")) {
 			Debug.Log ("Start Collide Cell");
 			this.gameObject.GetComponent<Rigidbody2D> ().isKinematic = true;
+			m_isOnCenterAnimation = true;
+			m_centerCell = other.gameObject.transform.position;
+			m_animationTime = 0;
+			toto = this.transform.position;
 		}
 	}
 }
