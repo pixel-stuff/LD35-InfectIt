7fde6d7a28201f6449cc6ad7deb10ce0c3d210fe
mathias <mathias@marbotic.fr>
2016-04-16 15:29:29 +0200

    laisse moi zumzum zen

---
+++ Assets/Script/Fight/FightManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightManager : MonoBehaviour {
+
+	public Sprite m_triangle;
+	public Sprite m_carre;
+	public Sprite m_rond;
+	public Sprite m_croix;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
