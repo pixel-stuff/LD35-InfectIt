cad4a4763b5ffb11b806f2bc714eda69fb3f4a46
mathias <mathias@marbotic.fr>
2016-04-17 14:22:53 +0200

    gros push de sa maman

--- Assets/Script/Fight/FightManager.cs
+++ Assets/Script/Fight/FightManager.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 [Serializable]
 public struct StepFight{
-	public string m_name;
-	public Sprite m_sprite;
+	public int m_id;	//0 -> triangle/up
+						//1 -> carre/left
+						//2 -> croix/down
+						//3 -> rond/right
+	public string m_nameController;
+	public Sprite m_spriteController;
+	public string m_nameKeyBoard;
+	public Sprite m_spriteKeyBoard;
 	public AudioClip m_sonReussi;
 	public AudioClip m_sonFailed;
 }
@@ -27,31 +35,59 @@ public class FightManager : MonoBehaviour {
 	}
 	#endregion Singleton
 	[Space(10)]
-	public StepFight[] m_listStep;
+	public StepFight[] m_listStep; 	
 	[Space(10)]
 	public GameObject[] m_listGameObjectDisplayable;
 
 	private bool m_isInit = false;
-
+	private int m_numberOfStep = 4;
+	private List<int> m_listOfInputWaited = new List<int> ();
+	private int temp = 0;
 
 	public void InitFight(Cell cell){
-		
+		TimeManager.m_instance.ChangeState (TimeState.fight);
+		m_listOfInputWaited.Clear();
+		for (int i = 0; i < m_listGameObjectDisplayable.Length; i++) {
+			if( i < m_numberOfStep){
+				temp = UnityEngine.Random.Range (0, 4);
+				m_listOfInputWaited.Add(temp);
+				m_listGameObjectDisplayable [i].GetComponent<Image> ().sprite = m_listStep[temp].m_spriteController;
+			}else{
+				m_listGameObjectDisplayable [i].SetActive (false);
+			}
+		}
+
+		for (int i = 0; i < m_listOfInputWaited.Count; i++) {
+			Debug.Log ("id : " + m_listOfInputWaited[i]);
+		}
+		FindObjectOfType<AudioManager> ().m_beatEvent += BeatInvokeHandle;
+		FindObjectOfType<AudioManager> ().PlayFightMusic ();
+		m_lastBeatInvoke = Time.time;
 
 		m_isInit = true;
 	}
 
 
+	private int m_numberOfBeatDoneInvoke = 0;
+	private float m_lastBeatInvoke = 0;
+	public void BeatInvokeHandle(){
+		m_numberOfBeatDoneInvoke++;
+		Debug.Log ("BEAT FIGHT");
+		m_lastBeatInvoke = Time.time;
+	}
+
 	// Use this for initialization
 	void Start () {
+		InitFight (null);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 	
 	}
 
 	public void UpInput(){
-		
 		Debug.Log ("FIGHT UP");
 	}
 
