10fa3c9eac612d07f78d4f2abc9a4067376b5e33
mathias <mathias@marbotic.fr>
2016-04-18 14:56:01 +0200

    push it

---
+++ Assets/VirusFightManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VirusFightManager : MonoBehaviour {
+
+	private Animation m_animation;
+
+	void Awake(){
+		m_animation = this.GetComponent<Animation> ();
+	}
+	// Use this for initialization
+	void Start () {
+		m_animation.Play ("IdleToSquare");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
