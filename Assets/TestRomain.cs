9dfa0767819af1a8660af8dd4f1602c3e279fe98
Romain Poumeyrol <romain.poumeyrol@wopata.com>
2016-04-18 17:58:17 +0200

    virus update

---
+++ Assets/TestRomain.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRomain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		FindObjectOfType<FightManager>().InitFight (null);
+		GameStateManager.m_instance.setGameState (GameState.Playing);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
