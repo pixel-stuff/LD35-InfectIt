2ce6b22737430b6d4e22680d3cc6ef8ca882e545
Jeremy Morel <jeremy.morel.jm@gmail.com>
2016-04-18 04:19:56 -0700

    Add arrow

--- Assets/followGameObject.cs
+++ Assets/followGameObject.cs
@@ -5,13 +5,15 @@ public class followGameObject : MonoBehaviour {
 
 	public GameObject followGO;
 
-	public float force;
+	public GameObject container;
 
+	public float force;
+	public float OldRotation = 0;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		OldRotation = 0;
 	}
 	
 	// Update is called once per frame
@@ -20,8 +22,12 @@ public class followGameObject : MonoBehaviour {
 
 			Vector2 moveVector = new Vector2 (followGO.transform.position.x, followGO.transform.position.y) - new Vector2 (this.transform.position.x, this.transform.position.y);
 			moveVector.Normalize ();
+
+			float angle = Mathf.Atan2(moveVector.y, moveVector.x);
+			angle = (-180.0f / Mathf.PI * angle)%360.0f;
 			this.gameObject.GetComponent<Rigidbody2D> ().AddRelativeForce (moveVector * force);
-			//this.transform.Rotate( Vector3.forward * Mathf.Acos (moveVector.x));
+			container.transform.Rotate( Vector3.forward * (OldRotation -angle));
+			OldRotation = angle;
 		} else {
 			this.gameObject.SetActive (false);
 		}
