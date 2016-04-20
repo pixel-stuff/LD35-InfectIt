6d619361edec5b5885b00ce8bc91565f5fe8e960
Jeremy Morel <jmorel@betomorrow.com>
2016-04-20 14:12:23 +0200

    Indicator pas sur le virus + tentative mobile Combat

---
+++ Assets/indicationInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class indicationInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TouchUp(){
+		PlayerManager.UpFight ();
+	}
+
+	public void TouchDown(){
+		PlayerManager.DownFight ();
+	}
+
+	public void TouchLeft(){
+		PlayerManager.LeftFight ();
+	}
+
+	public void TouchRight(){
+		PlayerManager.RightFight ();
+	}
+}
