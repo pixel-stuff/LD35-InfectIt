8b442ca1807baff5b5372b141803e690132c74ca
Jeremy Morel <jeremy.morel.jm@gmail.com>
2016-04-21 00:45:39 -0700

    optim ++ parralax

--- Assets/generateBG.cs
+++ Assets/generateBG.cs
@@ -6,14 +6,21 @@ using System.Collections.Generic;
 [System.Serializable]
 public class RandomizeGO : System.Object
 {
-	[Header("Parralax plan prefab")]
 	public GameObject randomGO;
 	public int popProbability;
+}
+
+[System.Serializable]
+public class ParralaxActivateGO : System.Object
+{	
+	public GameObject chooseGameObject;
+	public Vector3 position;
 }
 
 
 public class generateBG : MonoBehaviour {
 
+	public Camera m_camera;
 
 	public RandomizeGO[] randomGO;
 	public float fullinessFactor = 100;
@@ -36,6 +43,11 @@ public class generateBG : MonoBehaviour {
 
 	public Color parralaxColor;
 
+	public List<ParralaxActivateGO> m_parralaxList;
+
+	public Vector3 downLeft;
+	public Vector3 upRight;
+
 	// Use this for initialization
 	void Start () {
 		m_gameObject = new List<GameObject>();
@@ -49,9 +61,11 @@ public class generateBG : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Generate) {
-			generation ();
+			generateList ();
 			Generate = false;
 		}
+
+		generation ();
 	}
 
 
@@ -72,13 +86,39 @@ public class generateBG : MonoBehaviour {
 
 
 	void generation() {
-		if (m_gameObject.Count > 0) {
-			foreach (GameObject g in m_gameObject) {
-				Destroy (g);
-			}
-		}
-		m_iterator = downLeftLimit.transform.position;
 
+		if (m_parralaxList.Count > 0) {
+
+
+			List<ParralaxActivateGO> removeParralaxObject = new List<ParralaxActivateGO>();
+			float cameraOrthographiqueSize = m_camera.orthographicSize*2;
+			float CameraW = m_camera.rect.width;
+			float CameraH = m_camera.rect.height;
+		
+			upRight = new Vector3 (m_camera.transform.position.x + CameraW * cameraOrthographiqueSize, m_camera.transform.position.y + CameraH * cameraOrthographiqueSize,this.transform.position.z);
+			downLeft = new Vector3 (m_camera.transform.position.x - CameraW * cameraOrthographiqueSize, m_camera.transform.position.y - CameraH * cameraOrthographiqueSize,this.transform.position.z);
+
+			foreach(ParralaxActivateGO g in m_parralaxList) {
+				if (g.position.x > downLeft.x && g.position.x < upRight.x && g.position.y > downLeft.y && g.position.y < upRight.y) {
+					GameObject asset = Instantiate (g.chooseGameObject);
+					asset.transform.position = g.position;
+					asset.transform.parent = this.transform;
+					asset.GetComponentInChildren<randomMove> ().maxForce = maxForce;
+					asset.GetComponentInChildren<randomMove> ().minForce = maxForce;
+					asset.transform.localScale = new Vector3 (scale,scale,1);
+					asset.GetComponentInChildren<SpriteRenderer> ().color = parralaxColor;
+					m_gameObject.Add (asset);
+					removeParralaxObject.Add (g);
+				}
+					
+			}
+			if (removeParralaxObject.Count > 0) {
+				foreach (ParralaxActivateGO r in removeParralaxObject) {
+					m_parralaxList.Remove (r);
+				}
+			}
+
+			/*
 		while(m_iterator.y < UpRigthLimit.transform.position.y){
 			while (m_iterator.x < UpRigthLimit.transform.position.x) {
 				float random = Random.Range (0, 100f);
@@ -98,6 +138,33 @@ public class generateBG : MonoBehaviour {
 			}
 			m_iterator.x = downLeftLimit.transform.position.x;
 			m_iterator += genereStep () * Vector2.up;
+		}*/
 		}
+	}
+
+
+	void generateList (){
+		if (m_parralaxList.Count > 0) {
+			m_parralaxList.Clear();
+		}
+		m_iterator = downLeftLimit.transform.position;
+
+		while(m_iterator.y < UpRigthLimit.transform.position.y){
+			while (m_iterator.x < UpRigthLimit.transform.position.x) {
+				float random = Random.Range (0, 100f);
+				if(fullinessFactor>random) {
+					int AssetId = getIdOfNextAsset ();
+					ParralaxActivateGO parralaxGO = new ParralaxActivateGO();
+					parralaxGO.chooseGameObject = randomGO [AssetId].randomGO;
+					parralaxGO.position =  new Vector3 (m_iterator.x,m_iterator.y,this.transform.position.z);
+					m_parralaxList.Add (parralaxGO);
+				}
+
+				m_iterator += genereStep () * Vector2.right;
+			}
+			m_iterator.x = downLeftLimit.transform.position.x;
+			m_iterator += genereStep () * Vector2.up;
+		}
+
 	}
 }
